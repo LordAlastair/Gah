c644f3edf3de71d27623cf06076d190c494589e8
Harlan Gomes Nascimento <harlangomesnascimento@outlook.com>
2018-03-28 20:52:43 -0300

    Prototipagem das telas

--- app/src/main/java/com/gah/gah/MainActivity.java
+++ app/src/main/java/com/gah/gah/MainActivity.java
@@ -1,15 +1,20 @@
 package com.gah.gah;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.widget.TextView;
+import android.support.v4.app.Fragment;
 
-public class MainActivity extends AppCompatActivity {
-
-    private TextView mTextMessage;
+public class MainActivity extends AppCompatActivity
+        implements RelatorioFragment.OnFragmentInteractionListener {
+    Fragment fragment = null;
+    private FragmentTransaction mFragmentTransaction;
+    private FragmentManager mFragmentManager;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -17,17 +22,26 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
+                case R.id.navigation_inicio:
+
+                case R.id.navigation_cliente:
+
+                case R.id.navigation_horario:
+
+                case R.id.navigation_servico:
+
+                case R.id.navigation_relatorio:
+                    fragment = new RelatorioFragment();
             }
-            return false;
+            if(fragment != null){
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                transaction.add(R.id.fl_content, fragment);
+                transaction.addToBackStack(null);
+                transaction.commit();
+            }
+
+
+            return true;
         }
     };
 
@@ -36,9 +50,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
+    }
 }
